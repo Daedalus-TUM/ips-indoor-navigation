8e6fdf19131975c4e2f89e1490f4d2bd2c04c680
Qwip <qwip@suremail.info>
2014-03-27 18:28:36 +0100

    testing...

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,17 +2,12 @@ function Show_Stuff(Click_Menu)
 // Function that will swap the display/no display for
 // all content within span tags
 {
-  switch (Click_Menu)
+  if (Click_Menu.style.display == "none")
   {
-  case "hardware":
-    hardware.style.display = "";
-    software.style.display = "none";
-    break;
-  case "software":
-    hardware.style.display = "none";
-    software.style.display = "";
-    break;
-  case default:
-    break;
+     Click_Menu.style.display = "";
+  }
+  else
+  {
+     Click_Menu.style.display = "none";
   }
 }
