3eaa3a173198f2d34b15ffae86d1165e1f3bc1d7
Qwip <qwip@suremail.info>
2014-03-27 17:53:04 +0100

    testing...

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+function Show_Stuff(Click_Menu)
+// Function that will swap the display/no display for
+// all content within span tags
+{
+if (Click_Menu.style.display == "none")
+{
+Click_Menu.style.display = "";
+}
+else
+{
+Click_Menu.style.display = "none";
+}
+}
